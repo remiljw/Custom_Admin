2d7f0a4442d54d628c2a85f756a6ef494f2d78f8
Michael Ajanaku <remiljw@gmail.com>
2020-08-26 15:07:45 +0100

    button actions

--- usermetrics/admin.py
+++ usermetrics/admin.py
@@ -3,24 +3,45 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm 
 from django.contrib.auth.models import Group
-
+from django.utils.html import format_html
+from django.urls import reverse, path
+from django.conf.urls import url
+from django.utils.safestring import mark_safe
+from django.http import HttpResponseRedirect
 # Register your models here. 
 
 admin.site.unregister(Group)
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin):    
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['username','email', 'is_active',]
-    actions = ['is_not_active','is_active',]
+    list_display = ('username','email', 'is_active', 'user_status')
 
-    def is_not_active(self, request, queryset):
-        queryset.update(is_active=False)
-    is_not_active.short_description = "Deactivate Selected User(s)" 
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(r'^(?P<customuser_id>.+)/change/$', self.is_active, name='change'),
+        ]
+        return custom_urls + urls
 
-    def is_active(self, request, queryset):
-        queryset.update(is_active=True)
-    is_active.short_description = "Reactivate Selected User(s)"
+    def user_status(self, obj):
+        return format_html(
+            '<a class="button" href="{}"> Deactivate</a>&nbsp;'
+            '<a class="button" href="{}"> Reactivate</a>',
+            reverse('admin:change', args=[obj.pk]),reverse('admin:change', args=[obj.pk]),
+        )
+    user_status.allow_tags = True
+    user_status.short_description = 'Options'
 
+    def is_active(self, request, customuser_id, *args, **kwrags):
+        opt = customuser_id
+        user = CustomUser.objects.get(id=opt)
+        if user.is_active == True:
+            user.is_active=False
+            user.save()
+        else:
+            user.is_active=True
+            user.save()
+        return HttpResponseRedirect(reverse('../'))
 admin.site.register(CustomUser, CustomUserAdmin)
