0cd83e8fec4175bae9c4719ab8b242307d31aa0b
Michael Ajanaku <remiljw@gmail.com>
2020-08-26 16:38:36 +0100

    added user metrics

--- usermetrics/admin.py
+++ usermetrics/admin.py
@@ -1,29 +1,63 @@
+import json
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-from .forms import CustomUserCreationForm, CustomUserChangeForm 
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.functions import TruncDay
 from django.contrib.auth.models import Group
 from django.utils.html import format_html
 from django.urls import reverse, path
 from django.conf.urls import url
 from django.utils.safestring import mark_safe
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 # Register your models here. 
 
 admin.site.unregister(Group)
 
 class CustomUserAdmin(UserAdmin):    
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
     model = CustomUser
     list_display = ('username','email', 'is_active', 'user_status')
+    ordering=('date_joined',)
+    
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new subscribers per day
+        chart_data = (
+            CustomUser.objects.annotate(date=TruncDay("date_joined"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
 
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
             url(r'^(?P<customuser_id>.+)/change/$', self.is_active, name='change'),
+            path('chart_data/', self.admin_site.admin_view(self.chart_data_endpoint))
         ]
         return custom_urls + urls
+
+    def chart_data_endpoint(self, request):
+        chart_data = self.chart_data()
+        return JsonResponse(list(chart_data), safe=False)
+
+    def chart_data(self):
+        return (
+            CustomUser.objects.annotate(date=TruncDay("date_joined"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
 
     def user_status(self, obj):
         return format_html(
--- usermetrics/forms.py
+++ usermetrics/forms.py
@@ -1,15 +1,23 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+# from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
+# class CustomUserCreationForm(UserCreationForm):
 
-    class Meta:
-        model = CustomUser
-        fields =('username', 'email')
+#     class Meta:
+#         model = CustomUser
+#         fields =('username', 'email')
 
-class CustomUserChangeForm(UserChangeForm):
+# class CustomUserChangeForm(UserChangeForm):
 
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email')
+#     class Meta:
+#         model = CustomUser
+#         fields = ('username', 'email')
+
+# class SendEmailForm(forms.Form):
+#     subject = forms.CharField(
+#         widget=forms.TextInput(attrs={'placeholder': _('Subject')}))
+#     message = forms.CharField(widget=forms.Textarea)
+#     users = forms.ModelMultipleChoiceField(label="To",
+#                                            queryset=CustomUser.objects.all(),
+#                                            widget=forms.SelectMultiple())
